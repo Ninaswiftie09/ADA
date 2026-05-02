72229c772d0c53eca04c79dd550b28bd940369fe
Ninaswiftie09 <naj231088@uvg.edu.gt>
2026-05-01 22:14:16 -0600

    pequeño cambio y nuevo documento

--- Lab-API.py
+++ Lab-API.py
@@ -1,16 +1,18 @@
-import requests, os
+import requests
+import os
 
 API_KEY = os.environ.get("MAPS_KEY")
-resp = requests.get(
-    "https://maps.googleapis.com/maps/api/place/findplacefromtext/json",
-    params={
-        "input": "Torre del Reformador Guatemala",
-        "inputtype": "textquery",
-        "fields": "place_id,name,geometry,formatted_address",
-        "key": API_KEY,
+
+resp = requests.post(
+    "https://places.googleapis.com/v1/places:searchText",
+    headers={
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+        "X-Goog-FieldMask": "places.id,places.displayName,places.formattedAddress,places.location",
+    },
+    json={
+        "textQuery": "Torre del Reformador Guatemala"
     }
 )
 
 print(resp.json())
-
-
